0caa420e86ff3818aaabe7ae9ef4609295e464d3
Megan Ford <meg.c.ford@gmail.com>
2016-04-20 08:37:49 -0500

    tree creation loop

--- quiz.js
+++ quiz.js
@@ -1,25 +1,55 @@
-/*Instructions
 
-Create a tree function that should build a pine tree out of a character in the Chrome dev tools console. It accepts a single object as an argument. The object should have two key/value pairs.
-
-
-.
-Once the user enters in a number, and a character, the user can either then just press the enter key (as long as the cursor is in one of the input fields), or click a button that is labeled "Grow your tree" and the tree should be shown in the console. This requires you to add an event listener to the button, as well as an event listener for the enter/return key.
-
-If either of the input fields does not have a value in it when the user presses the enter key, or presses the button, then display an alert stating that both fields must have a value.
-
- */
 //create button
 var button = document.getElementById("growYourTree");
-
-var runTreeGrower = function() {
-//this part gets the keys from the dom. 
-var treeSpecs = { 
-  height: parseInt(document.getElementById("treeHeight").value), 
-  character: document.getElementById("treeCharacter").value
-  }
-  console.log("making sure button works", treeSpecs.character); 
+//create user spec object so it can be used in all the functions.
+var treeSpecs = {
+  height:  "",
+  character:  ""
 }
+var stackofTree = [];
+
+
+//EVENT HANDLER//
+//
+var runTreeGrower = function() {
+//this part gets the keys from the dom. Inside the function so it will get the user's input and not a blank.
+  treeSpecs.height = parseInt(document.getElementById("treeHeight").value), 
+  treeSpecs.character = document.getElementById("treeCharacter").value
+
+  console.log("making sure button works", treeSpecs.height, treeSpecs.character);
+
+ //after key getting, this function runs to make sure the keys aren't blank.
+  runIfPopulated();
+}
+
+ //this function prevents the tree creator from running unless  both fields are populated.
+ var runIfPopulated = function() {
+  if (treeSpecs.height == "" || treeSpecs.character == "") {
+    alert("Both fields must have a value.")
+  } else {
+  
+    console.log("making sure button works INSIDE IF STATEMENT", treeSpecs.height, treeSpecs.character);
+
+    //treemaker function.
+    letsMakeSomeTrees();
+
+  }
+}
+
+//ok I need to make two nested for loops
+
+var letsMakeSomeTrees = function() {
+  
+  for (var i = 1; i < treeSpecs.height; i++) {
+    var output = [];
+    output.unshift(" ".repeat(treeSpecs.height-i)); 
+    output.push(treeSpecs.character.repeat(i+(i-1)));
+    output = output.join("")
+    console.log(output);
+  }
+}
+//still need to make the event listener that makes "enter" work. again.
 
 //submit button listener   
 button.addEventListener("click", runTreeGrower);
+
